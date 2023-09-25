a6cf3cf7f19d4fa206a3c6f3ccfca5aa787ab4ef
Momboz12 <claudio.operti@gmail.com>
2023-09-25 06:37:33 -0300

    Agrego controlador UsersController del modelo generado

---
+++ MVC/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            var user = new UserViewModel
+            {
+                Name = "Claudio D. Operti",
+                Email = "claudio.operti@cablasociados.com",
+                Phone = "353-5084611"
+            };
+
+            return View(user);
+        }
+    }
+}
